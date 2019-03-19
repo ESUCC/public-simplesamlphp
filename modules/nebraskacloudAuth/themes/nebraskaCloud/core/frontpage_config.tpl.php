6537b71b3e709ab5923be608dcfee8e63b01e1cb
Adam W Zheng <Venator-Fox@users.noreply.github.com>
2019-03-19 16:09:33 -0500

    Fix the mini enablebox caused by #32

--- modules/nebraskacloudAuth/themes/nebraskaCloud/core/frontpage_config.tpl.php
+++ modules/nebraskacloudAuth/themes/nebraskaCloud/core/frontpage_config.tpl.php
@@ -5,7 +5,6 @@ $this->includeAtTemplateBase('includes/header.php');
 
 ?>
 
-
 <!-- 
 <div id="tabdiv">
 <ul>
@@ -21,34 +20,27 @@ if ($this->data['isadmin']) {
 }
 ?>
 
-
-
-
 <div style="float:right">
 	<code style="background: white; background: #f5f5f5; border: 1px dotted #bbb; padding: 1em;  color: #555" ><?php 
         echo "SimpleSAMLphp " . $this->data['version'];
 	?></code>
 </div>
 
-
-
 <div style="clear: both; float: right; margin-top: 1em;" class="enablebox mini">
-	<table>
-	
-	<?php
-	$icon_enabled  = '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/accept.png" alt="enabled" />';
-	$icon_disabled = '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/delete.png" alt="disabled" />';
-	?>
-	
-		<tr class="<?php echo $this->data['enablematrix']['saml20-idp'] ? 'enabled' : 'disabled'; ?>"><td>SAML 2.0 IdP</td>
-			<td><?php echo $this->data['enablematrix']['saml20-idp'] ? $icon_enabled : $icon_disabled; ?></td></tr>
-			
-		<tr class="<?php echo $this->data['enablematrix']['shib13-idp'] ? 'enabled' : 'disabled'; ?>"><td>Shib 1.3 IdP</td>
-			<td><?php echo $this->data['enablematrix']['shib13-idp'] ? $icon_enabled : $icon_disabled; ?></td></tr>
-		
-	</table>
-</div>
+    <table>
 
+    <?php
+    $icon_enabled  = '<img src="/'.$this->data['baseurlpath'].'resources/icons/silk/accept.png" alt="enabled" />';
+    $icon_disabled = '<img src="/'.$this->data['baseurlpath'].'resources/icons/silk/delete.png" alt="disabled" />';
+    echo '<tr class="'.($this->data['enablematrix']['saml20idp'] ? 'enabled' : 'disabled').'"><td>SAML 2.0 IdP</td>';
+    echo '<td>'.($this->data['enablematrix']['saml20idp'] ? $icon_enabled : $icon_disabled).'</td></tr>';
+
+    echo '<tr class="'.($this->data['enablematrix']['shib13idp'] ? 'enabled' : 'disabled').'"><td>Shib 1.3 IdP</td>';
+    echo '<td>'.($this->data['enablematrix']['shib13idp'] ? $icon_enabled : $icon_disabled).'</td></tr>';
+    ?>
+
+    </table>
+</div>
 
 
 <h2><?php echo $this->t('{core:frontpage:configuration}'); ?></h2>
@@ -76,26 +68,22 @@ if ($this->data['isadmin']) {
 	}
 ?>
 <?php 
-if ($this->data['isadmin']) {
 
-	echo '<h2>'. $this->t('{core:frontpage:checkphp}') . '</h2>';
-	echo '<div class="enablebox"><table>';
-	
-	
-	$icon_enabled  = '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/accept.png" alt="enabled" />';
-	$icon_disabled = '<img src="/' . $this->data['baseurlpath'] . 'resources/icons/silk/delete.png" alt="disabled" />';
-	
-	
-	foreach ($this->data['funcmatrix'] AS $func) {
-		echo '<tr class="' . ($func['enabled'] ? 'enabled' : 'disabled') . '"><td>' . ($func['enabled'] ? $icon_enabled : $icon_disabled) . '</td>';
-		echo '<td>' . $this->t($this->data['requiredmap'][$func['required']]) . '</td><td>' . $func['descr'] . '</td></tr>';
-	}
-	echo('</table></div>');
+if ($this->data['isadmin']) {
+    echo '<h2>'.$this->t('{core:frontpage:checkphp}').'</h2>';
+    echo '<div class="enablebox"><table>';
+
+    $icon_enabled = '<img src="/'.$this->data['baseurlpath'].'resources/icons/silk/accept.png" alt="enabled" />';
+    $icon_disabled = '<img src="/'.$this->data['baseurlpath'].'resources/icons/silk/delete.png" alt="disabled" />';
+
+    foreach ($this->data['funcmatrix'] as $func) {
+        echo '<tr class="'.($func['enabled'] ? 'enabled' : 'disabled').'"><td>'.
+            ($func['enabled'] ? $icon_enabled : $icon_disabled).'</td>';
+        echo '<td>'.$this->t($this->data['requiredmap'][$func['required']]).'</td><td>'.$func['descr'].'</td></tr>';
+    }
+    echo '</table></div>';
 }
 
 ?>
 	
-	
-
-		
 <?php $this->includeAtTemplateBase('includes/footer.php');
